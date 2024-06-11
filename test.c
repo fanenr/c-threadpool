75562270d159fb333e33d3dcf6ffec5fbe11e90e
Arthur63 <arthur63@163.com>
2024-06-11 15:32:47 +0800

    use atomic variable to count tasks

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
 #include "threadpool.h"
 
 #include <stdio.h>
-#include <unistd.h>
 
+size_t counter;
 static void task (void *arg);
 
 int
 main (void)
 {
   threadpool_t pool;
-  size_t tasks = 1000;
+  size_t tasks = 100000;
 
   if (0 != threadpool_init (&pool, 16))
     return 1;
@@ -19,11 +19,12 @@ main (void)
       break;
 
   threadpool_free (&pool);
+  printf ("counter: %lu\n", counter);
 }
 
 static void
 task (void *arg)
 {
   size_t i = (size_t)arg;
-  printf ("%lu ", i);
+  __atomic_fetch_add (&counter, 1, __ATOMIC_RELAXED);
 }
